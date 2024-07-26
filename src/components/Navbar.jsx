452d4ad0f5282cb44c9b7f1f2af4080c436c7f97
emmanuelle <emmanuelledennemont@orange.fr>
2024-07-26 15:33:33 +0200

    logout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,26 @@
-import Wrapper from "../assets/wrappers/Navbar"
-import {FaAlignLeft} from "react-icons/fa"
-import Logo from "./Logo"
-import { useDashboardContext } from "../pages/DashboardLayout"
+import Wrapper from '../assets/wrappers/Navbar';
+import { FaAlignLeft } from 'react-icons/fa';
+import Logo from './Logo';
+import { useDashboardContext } from '../pages/DashboardLayout';
+import LogoutContainer from './LogoutContainer';
 
 const Navbar = () => {
-
-  const {toggleSidebar} = useDashboardContext()
-
+  const { toggleSidebar } = useDashboardContext();
   return (
     <Wrapper>
-      <div className="nav-center">    
-          <button type="button" className="toggle-btn" onClick={toggleSidebar} >
-            <FaAlignLeft />
-          </button>
-          <div>
-            <Logo />
-            <h4 className="logo-text">
-              Dashboard
-            </h4>
-          </div>
-          <div className="btn-container">toggle/logout</div>
+      <div className="nav-center">
+        <button type="button" className="toggle-btn" onClick={toggleSidebar}>
+          <FaAlignLeft />
+        </button>
+        <div>
+          <Logo />
+          <h4 className="logo-text">dashboard</h4>
+        </div>
+        <div className="btn-container">
+          <LogoutContainer />
+        </div>
       </div>
     </Wrapper>
-  )
-}
-
-export default Navbar
+  );
+};
+export default Navbar;
