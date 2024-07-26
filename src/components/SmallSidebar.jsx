f5555766f7e3211f3851ba69b2c04ab33b82990d
emmanuelle <emmanuelledennemont@orange.fr>
2024-07-26 15:08:02 +0200

    show sidebar

--- src/components/SmallSidebar.jsx
+++ src/components/SmallSidebar.jsx
@@ -1,14 +1,42 @@
-import Wrapper from "../assets/wrappers/SmallSidebar"
-import { useDashboardContext } from "../pages/DashboardLayout"
+import Wrapper from '../assets/wrappers/SmallSidebar';
+import { useDashboardContext } from '../pages/DashboardLayout';
+import { FaTimes } from 'react-icons/fa';
+import Logo from './Logo';
+import { NavLink } from 'react-router-dom';
+import links from '../utils/links';
 
 const SmallSidebar = () => {
-  const data = useDashboardContext()
-  console.log(data)
-
+  const { showSidebar, toggleSidebar } = useDashboardContext();
 
   return (
-    <Wrapper>SmallSidebar</Wrapper>
-  )
-}
+    <Wrapper>
+      <div
+        className={
+          showSidebar ? 'sidebar-container show-sidebar' : 'sidebar-container'
+        }
+      >
+        <div className="content">
+          <button type="button" className="close-btn" onClick={toggleSidebar}>
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <div className="nav-links">
+            {links.map((link) => {
+              const { path, text, icon } = link;
+              return (
+                <NavLink to={path} key={text} className="nav-link">
+                  <span className="icon">{icon}</span>
+                  {text}
+                </NavLink>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
 
-export default SmallSidebar
+export default SmallSidebar;
--- src/utils/links.jsx
+++ src/utils/links.jsx
@@ -1,3 +1,6 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
 import { IoBarChartSharp } from 'react-icons/io5';
 import { MdQueryStats } from 'react-icons/md';
 import { FaWpforms } from 'react-icons/fa';
